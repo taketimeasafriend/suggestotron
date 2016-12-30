826546df5b06e8fc2875393269a3fcbcdaa6da2d
miaoxiaoge <taketimeasafriend@gmail.com>
2016-12-31 02:13:47 +0800

    add about page and debug

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
       post 'upvote'
     end
   end
+  get "/pages/:page" => "pages#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
